112160d3e4bd9f9ce2895074873c8d2554898664
jiaying.lu <lujiaying93@foxmail.com>
2016-05-12 00:54:57 +0800

    增加说明和log工具类

---
+++ utils/log_tool.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+'''
+Easy and basic configure for print log
+
+Author: lujiaying
+Date: 2016/05/12
+'''
+
+import logging
+from logging.handlers import RotatingFileHandler
+import os
+
+# 用以控制是否输出到屏幕，线上环境不输出到屏幕
+DebugConf = True
+#DebugConf = False
+
+abs_path = os.path.dirname(os.path.abspath(__file__))
+abs_father_path = os.path.dirname(abs_path)
+log_dir_path = abs_father_path + '/log'
+if not os.path.exists(log_dir_path):
+    os.makedirs(log_dir_path)
+
+model_logger = logging.getLogger('model')
+
+formatter = logging.Formatter('[%(asctime)s][pid:%(process)s] %(module)s.%(funcName)s: %(levelname)s: %(message)s')
+
+# StreamHandler for print log to console
+hdr = logging.StreamHandler()
+hdr.setFormatter(formatter)
+hdr.setLevel(logging.DEBUG)
+
+# RotatingFileHandler
+fhr_model = RotatingFileHandler('%s/model.log'%(log_dir_path), maxBytes=10*1024*1024, backupCount=3)
+fhr_model.setFormatter(formatter)
+fhr_model.setLevel(logging.DEBUG)
+
+model_logger.addHandler(fhr_model)
+model_logger.addHandler(hdr)
+model_logger.setLevel(logging.DEBUG)
+
+if __name__ == '__main__':
+    '''
+    Usage:
+
+    from utils.log_tool import model_logger as logger
+    '''
+    model_logger.debug('test from model_logger')
