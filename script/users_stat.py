6133755176f6fc52df4f1a1f5c9a94e9a03cf0bb
jayvee-Air <jayveehe@gmail.com>
2016-05-13 01:53:12 +0800

    取消使用log记录

--- script/users_stat.py
+++ script/users_stat.py
@@ -22,16 +22,24 @@ def user_actions_stats(fout_name):
         date_create = Arrow.fromtimestamp(gmt_create)
         date_str = date_create.format('YYYY-MM-DD')
         user_dict[user_id][date_str][action_type] += 1
-    model_logger.debug('total users:%s' % len(user_dict))
+        count += 1
+        if count % 10000 == 0:
+            print 'handled %s records' % count
+    print 'total users:%s' % len(user_dict)
     # 输出结果到feature文件夹中
     with open('%s/feature/%s.csv' % (PROJECT_PATH, fout_name), 'w') as fout:
         fout.write('user_id,date_str,plays,downloads,favors\n')
+        count = 0
         for user_id in user_dict.keys():
             user_act = user_dict[user_id]
             for date_str in user_act.keys():
                 acts = user_act[date_str]
                 fout.write('%s,%s,%s,%s,%s\n' % (user_id, date_str, acts['1'], acts['2'], acts['3']))
-        model_logger.debug('user actions stats done.')
+            count += 1
+            progress = 100.0 * count / float(len(user_dict))
+            if progress % 5 == 0:
+                print '%s done' % progress
+        print 'user actions stats done.'
 
 
 if __name__ == '__main__':
