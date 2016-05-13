7ad78294519df30a03383417876e4557db583897
jiaying.lu <lujiaying93@foxmail.com>
2016-05-13 10:44:00 +0800

    Update log_tool

--- utils/log_tool.py
+++ utils/log_tool.py
@@ -20,6 +20,7 @@ log_dir_path = abs_father_path + '/log'
 if not os.path.exists(log_dir_path):
     os.makedirs(log_dir_path)
 
+feature_logger = logging.getLogger('feature')
 model_logger = logging.getLogger('model')
 
 formatter = logging.Formatter('[%(asctime)s][pid:%(process)s] %(module)s.%(funcName)s: %(levelname)s: %(message)s')
@@ -34,9 +35,18 @@ fhr_model = RotatingFileHandler('%s/model.log'%(log_dir_path), maxBytes=10*1024*
 fhr_model.setFormatter(formatter)
 fhr_model.setLevel(logging.DEBUG)
 
+# RotatingFileHandler
+fhr_feature = RotatingFileHandler('%s/feature.log'%(log_dir_path), maxBytes=10*1024*1024, backupCount=3)
+fhr_feature.setFormatter(formatter)
+fhr_feature.setLevel(logging.DEBUG)
+
 model_logger.addHandler(fhr_model)
 model_logger.addHandler(hdr)
 model_logger.setLevel(logging.DEBUG)
+
+feature_logger.addHandler(fhr_feature)
+feature_logger.addHandler(hdr)
+feature_logger.setLevel(logging.DEBUG)
 
 if __name__ == '__main__':
     '''
@@ -45,3 +55,4 @@ if __name__ == '__main__':
     from utils.log_tool import model_logger as logger
     '''
     model_logger.debug('test from model_logger')
+    feature_logger.debug('test from feature_logger')
